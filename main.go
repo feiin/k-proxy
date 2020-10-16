0934c397a627ef8f7b3bc63227c4cf63fd799b27
solar <feiin@qq.com>
2020-10-16 13:50:20 +0800

    fixed

--- main.go
+++ main.go
@@ -61,7 +61,7 @@ func ReverseProxy(targets []*url.URL) *httputil.ReverseProxy{
 		if ct, ok := localAddr.(interface{ Increment() int }); ok {
 			if ct.Increment() >= maxRequestsPerCon {
 				// req.Header("Connection", "close")
-				fmt.Printf("arrive at 1000 requests")
+				fmt.Printf("arrive at %d requests",maxRequestsPerCon)
 				req.Header["Connection"] = []string{"close"}
 
 			}
